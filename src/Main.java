b8956408998557d8632ea87d5820fc936c11e540
Куликов Валерий Романович <VRKulikov@beeline.ru>
2024-10-24 11:02:02 +0300

    Task-1: Defined condition to resolve mobile os system

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte logFalse = 0;
+        byte logTrue = 1;
+        byte clientOS = getRandomNumber(logFalse, logTrue);
+        boolean isAndroid = clientOS == 1;
+        boolean isIOS = clientOS == 0;
+
+        if (isAndroid) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (isIOS) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+    }
+
+    public static byte getRandomNumber(byte min, byte max) {
+        return (byte) (Math.floor((float) Math.random() * (max - min + 1)) + min);
     }
 }
